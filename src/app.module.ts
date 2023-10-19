781f6e98484ee6d09106f79e4d9b498993b946ba
Orlando Rojas <josuerojas161296@gmail.com>
2023-10-19 17:50:24 -0400

    agregando la configuracion en las variables de entorno y habilitando el puerto por defecto en railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,9 @@ import { log } from 'console';
 
     ConfigModule.forRoot(),
 
-    MongooseModule.forRoot( process.env.MONGO_URI ),
+    MongooseModule.forRoot( process.env.MONGO_URI,{
+      dbName: process.env.MONGO_DB_NAME
+    } ),
     AuthModule
   ],
   controllers: [],
